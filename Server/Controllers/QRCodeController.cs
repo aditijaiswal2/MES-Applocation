43342839db43609aeefed42432051c5de4b4e6a3
aditi jaiswal <aditi.jaiswal@axiscades.in>
2025-04-14 22:25:17 +0530

    Incoming ADD Images

--- Server/Controllers/QRCodeController.cs
+++ Server/Controllers/QRCodeController.cs
@@ -8,6 +8,7 @@ using MES.Shared.Models;
 using ZXing.QrCode;
 using MES.Shared.Models.Rotors;
 using static MES.Client.Dialog.Rotors.PreviewDialog;
+using MES.Shared.DTOs;
 
 namespace MES.Server.Controllers
 {
@@ -66,55 +67,55 @@ namespace MES.Server.Controllers
             }
         }
 
-        [HttpPost("IncomlocQR")]
-        public IActionResult GenerateLocQRCodelog([FromBody] QRRequestDTO request)
-        {
-            try
-            {
+        //[HttpPost("IncomlocQR")]
+        //public IActionResult GenerateLocQRCodelog([FromBody] QRRequestDTO request)
+        //{
+        //    try
+        //    {
 
 
-                var qrText = $"Serial Number: {request.SerialNumber},Module: {request.Module}, Customer: {request.Customer}, Date: {request.Date}";
-                var width = 250;
-                var height = 250;
-                var margin = 0;
-                var qrCodeWriter = new BarcodeWriterPixelData
-                {
-                    Format = ZXing.BarcodeFormat.QR_CODE,
-                    Options = new QrCodeEncodingOptions
-                    {
-                        Height = height,
-                        Width = width,
-                        Margin = margin
-                    }
-                };
-                var pixelData = qrCodeWriter.Write(qrText);
+        //        var qrText = $"Serial Number: {request.SerialNumber},Module: {request.Module}, Customer: {request.Customer}, Date: {request.Date}";
+        //        var width = 250;
+        //        var height = 250;
+        //        var margin = 0;
+        //        var qrCodeWriter = new BarcodeWriterPixelData
+        //        {
+        //            Format = ZXing.BarcodeFormat.QR_CODE,
+        //            Options = new QrCodeEncodingOptions
+        //            {
+        //                Height = height,
+        //                Width = width,
+        //                Margin = margin
+        //            }
+        //        };
+        //        var pixelData = qrCodeWriter.Write(qrText);
 
-                using (var bitmap = new System.Drawing.Bitmap(pixelData.Width, pixelData.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb))
-                {
-                    var bitmapData = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, pixelData.Width, pixelData.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
-                    try
-                    {
-                        System.Runtime.InteropServices.Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
-                    }
-                    finally
-                    {
-                        bitmap.UnlockBits(bitmapData);
-                    }
+        //        using (var bitmap = new System.Drawing.Bitmap(pixelData.Width, pixelData.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb))
+        //        {
+        //            var bitmapData = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, pixelData.Width, pixelData.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+        //            try
+        //            {
+        //                System.Runtime.InteropServices.Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
+        //            }
+        //            finally
+        //            {
+        //                bitmap.UnlockBits(bitmapData);
+        //            }
 
-                    using (var ms = new MemoryStream())
-                    {
-                        bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
-                        var byteArray = ms.ToArray();
-                        // Return the QR code image to the client
-                        return File(byteArray, "image/png");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                // Handle exception
-                return StatusCode(500, $"An error occurred: {ex.Message}");
-            }
-        }
+        //            using (var ms = new MemoryStream())
+        //            {
+        //                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+        //                var byteArray = ms.ToArray();
+        //                // Return the QR code image to the client
+        //                return File(byteArray, "image/png");
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Handle exception
+        //        return StatusCode(500, $"An error occurred: {ex.Message}");
+        //    }
+        //}
     }
 }
---
+++ Shared/DTOs/QRRequestDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MES.Shared.DTOs
+{
+    public class QRRequestDTO
+    {
+        public string SerialNumber { get; set; }
+        public string Module { get; set; }
+        public string Customer { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
